37ae6582afb84fa5a579481aa7c1f5d1a6490633
mppavan05 <mppavan05@gmail.com>
2022-06-04 13:21:50 +0530

    screen set up

---
+++ 3D-Balance/Assets/Scripts/Following.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Following : MonoBehaviour
+{
+    Camera cam;
+    Collider plaincollider;
+    RaycastHit hit;
+    Ray ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        plaincollider = GameObject.Find("Base").GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider == plaincollider)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * 5);
+            }
+        }
+    }
+}
