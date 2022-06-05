88061ffd89ddbc00e4ab3f0af741130fe354c955
mppavan05 <mppavan05@gmail.com>
2022-06-05 20:18:34 +0530

    gyro control

---
+++ 3D-Balance/Assets/Scripts/Balls.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    public GameObject[] myObjects;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            int RandomIndex = Random.Range(0, myObjects.Length);
+            Vector3 RandomSpwaner = new Vector3(Random.Range(-5, 5), 15, Random.Range(-5, 5));
+
+            Instantiate(myObjects[RandomIndex], RandomSpwaner, Quaternion.identity);
+        }
+        
+    }
+}
